563e6f874a7844b8e595e6bf44322b39029ff0d5
chinmaye-21 <chinmayeepuppy@gmail.com>
2026-02-05 10:43:13 +0530

    Add files via upload

---
+++ Day-3,Task-2,Multiple_Inheritance.py
@@ -0,0 +1,42 @@
+# Parent class
+class Product:
+    def __init__(self, product_name, price):
+        self.product_name = product_name
+        self.price = price
+
+    def display_product(self):
+        print("Product Name:", self.product_name)
+        print("Price:", self.price)
+
+
+# Child class
+class ElectronicProduct(Product):
+    def __init__(self, product_name, price, brand, warranty):
+        super().__init__(product_name, price)
+        self.brand = brand
+        self.warranty = warranty
+
+    def display_electronic_product(self):
+        self.display_product()
+        print("Brand:", self.brand)
+        print("Warranty:", self.warranty)
+
+
+# Grandchild class
+class MobilePhone(ElectronicProduct):
+    def __init__(self, product_name, price, brand, warranty, ram, storage):
+        super().__init__(product_name, price, brand, warranty)
+        self.ram = ram
+        self.storage = storage
+
+    def display_mobile_details(self):
+        self.display_electronic_product()
+        print("RAM:", self.ram)
+        print("Storage:", self.storage)
+
+
+# Creating object of MobilePhone
+mobile = MobilePhone("Smartphone", 25000, "OnePlus", "1 Year", "8 GB", "128 GB")
+
+# Displaying all details
+mobile.display_mobile_details()
